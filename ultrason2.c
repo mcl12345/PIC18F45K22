9fb722beda958ff025c4901356cfd7853f40d2f2
sb <s_b_@openmailbox.org>
2017-11-25 15:06:03 +0100

    comments added

--- ultrason2.c
+++ ultrason2.c
@@ -6,17 +6,18 @@ void main() {
   TRISC = 0;           // set direction to be output
   TRISD = 0;           // set direction to be output
   TRISE = 0xFF;        // set direction to be input
-  RE1_bit = 1;
+  RE1_bit = 1;         // On met RE1 à 1 pour envoyer le signal du trigger
 
   LATA =  0x00;
   LATB =  0x00;
   LATC =  0x00;
   LATD =  0x00;
 
-  while ( RE0_bit & 1 ) {
+  while ( RE0_bit & 1 ) {               // Réception de l'écho
+    // On commence à compter
     compteur = compteur + 1;
 
-    LATA =  0xFF;
-    LATD =  compteur * 10 /58;
+    LATA =  0xFF;                      // On envoit le signal pour voir qu'on bien dans la boucle
+    LATD =  compteur * 10 /58;         // On trasforme le compteur en distance en cm
   }
 }
